b048295775e3fb6cc42a74d5c1e171b5ade3a817
Bubbelbad <128900548+Bubbelbad@users.noreply.github.com>
2023-05-12 14:25:40 +0200

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -10,3 +10,16 @@ def calculate_expenses(plane_ticket_price, car_rental_rate, hotel_rate, trip_tim
   hotel_total = hotel_rate * trip_time - 10
   print(car_rental_total + hotel_total + plane_ticket_price)
 calculate_expenses(200, 100, 100, 5)
+
+#Budget program:
+current_budget = 3500.75
+def print_remaining_budget(budget):
+  print("Your remaining budget is: $" + str(budget))
+print_remaining_budget(current_budget)
+ 
+def deduct_expense(budget, expense):
+  return budget - expense
+shirt_expense = 9
+
+new_budget_after_shirt = deduct_expense(current_budget, shirt_expense)
+print_remaining_budget(new_budget_after_shirt)
