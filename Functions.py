8b7eb48a8013c141f32f7aaf88de52fdd6845eeb
Bubbelbad <128900548+Bubbelbad@users.noreply.github.com>
2023-05-20 14:39:27 +0200

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -51,3 +51,32 @@ def destination_setup(origin, destination, estimated_time, mode_of_transport = "
   print("You will be traveling by " + mode_of_transport)
   print("It will take approximately " + str(estimated_time) + " hours")
 destination_setup("Göteborg", "Mazatlan", estimate, "Airplane")
+
+#Functions practice in cordecademy: 
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+
+#Calculate fahrenheit to C
+def f_to_c(f_temp):
+  c_temp = (f_temp - 32) * 5/9
+  return c_temp
+f100_in_celsius = f_to_c(100)
+
+def c_to_f(c_temp):
+  f_temp = c_temp * 5/9 + 32
+  return f_temp
+c0_in_fahrenheit = c_to_f(0)
+print(c0_in_fahrenheit)
+
+def get_force(mass, acceleration):
+  return mass * acceleration
+train_force = get_force(train_mass, train_acceleration)
+print(train_force)
+print("GE train supplies " + str(train_force) + " Newtons of force.")
+
+def get_energy(mass, c=3*10**8):
+  return mass * c**2
+bomb_energy = get_energy(bomb_mass)
+print("A 1kg bomb supplies " + str(bomb_energy) + " Joules.")
